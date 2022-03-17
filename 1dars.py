bb1553112537819f40111085f29bd1b485b7dfce
Umarjon <sharofiddinov.07@mail.ru>
2022-03-17 15:11:58 +0500

    mini admin authentication (homework)

--- 1dars.py
+++ 1dars.py
@@ -5,14 +5,31 @@ c=str('test') #str---string satr yoki matin turi
 d=(a)
 print(type(d))
 """
-#magazin calculator
-a=str(input('mahsulot nomi: '))
-b=int(input('mahsulot massasi (kg): '))
-kartoshka=int(15000)
-sabzi=int(2000)
+
+
+# magazin calculator
+"""
+a = str(input('mahsulot nomi: '))
+b = int(input('mahsulot massasi (kg): '))
+kartoshka = int(15000)
+sabzi = int(2000)
 if a == 'kartoshka':
-     c=b*kartoshka
-     print(c,'so`m')
-elif a=='sabzi':
-     c=b*sabzi
-     print(c,'so`m')
+    c = b*kartoshka
+    print(c, 'so`m')
+elif a == 'sabzi':
+    c = b*sabzi
+    print(c, 'so`m')
+"""
+#mini admin authentication (homework)
+login = 'UZBase'
+password = 'admin'
+error = 'Login yoki parolda xatolik bor!!!'
+LoginKirish = str(input('Logini kiriting: '))
+PasswordKirish = str(input('Parolni kiriting: '))
+
+if LoginKirish == login:
+    print(PasswordKirish)
+if PasswordKirish == password:
+    print('Success!')
+else:
+    print(error)
