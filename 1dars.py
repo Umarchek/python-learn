83b48cce1a4b955959c32823a249f399452bd7c8
Umarjon <sharofiddinov.07@mail.ru>
2022-03-17 15:05:33 +0500

    created mini calculator for market

--- 1dars.py
+++ 1dars.py
@@ -1,5 +1,18 @@
+"""
 a=int(2) #int---integer butun son
 b=float(1.2) #float o'ngli kast sonlarni o'z ichiga oladigan tur
 c=str('test') #str---string satr yoki matin turi
 d=(a)
 print(type(d))
+"""
+#magazin calculator
+a=str(input('mahsulot nomi: '))
+b=int(input('mahsulot massasi (kg): '))
+kartoshka=int(15000)
+sabzi=int(2000)
+if a == 'kartoshka':
+     c=b*kartoshka
+     print(c,'so`m')
+elif a=='sabzi':
+     c=b*sabzi
+     print(c,'so`m')
