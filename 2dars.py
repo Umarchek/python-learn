3786890ad515dc13b707dcdd454cbd0a7f5e0b55
Umarjon <sharofiddinov.07@mail.ru>
2022-03-18 22:46:12 +0500

    Update 2dars.py

---
+++ 2dars.py
@@ -0,0 +1,17 @@
+a=5 #int - intager butun son
+b=5 #float 10lik kasr son
+#print(a//b)
+
+"""
+if a<b:
+	print('a b dan kichik')
+	
+elif a>b:
+	print('a b dan katta')
+
+if a==b:
+	print(a ,"va",b,'biriga teng')
+
+elif a!=b:
+	print('a va b biriga teng emas ')
+"""
