418e424aff04077e4264af4e2d1d8596e77f35bd
Umarjon <sharofiddinov.07@mail.ru>
2022-03-19 22:10:05 +0500

    Update 1dataTypes.py

--- 1dataTypes.py
+++ 1dataTypes.py
@@ -1,6 +1,7 @@
 #Name = False #boolean #logical meaning
 #Name = 22 #intager
 #Name = 2.2 #Float
-Name = 2+3j #complex
+#Name = 2+3j #complex
+Name = 'Alex' #string
 
 print(type(Name)) #Check type 
