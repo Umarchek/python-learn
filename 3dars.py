0e395735efb1e54452f2804a17f19843f58ffbba
Umarjon <sharofiddinov.07@mail.ru>
2022-03-20 13:45:57 +0500

    Update 3dars.py

--- 3dars.py
+++ 3dars.py
@@ -1,3 +1,19 @@
-
+"""
 a=['olma','banan','nok'] #Array
 print(a[0])
+
+#CheckDays
+oy=int(input('oyning tartib raqamini kiriting: '))
+if oy==2:
+	kun=28
+elif oy in [1,3,5,7,8,10,12]:
+	kun=31
+else :
+	kun=30
+print(oy,'oy ',kun,'kundan iborat')
+"""
+
+k=1
+while k<=10:
+	print(k)
+	k+=2 #kga 2ni qo'shish
