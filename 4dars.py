8de33f44d0ab94006a9eb397838e9bd9603c82fe
Umarjon <sharofiddinov.07@mail.ru>
2022-03-22 22:12:18 +0500

    Update 4dars.py

--- 4dars.py
+++ 4dars.py
@@ -1,4 +1,8 @@
-a="""Salom dunyo 
-bu ko'p qatorli 
-satr"""
-print(a)
+# a="""Salom dunyo 
+# bu ko'p qatorli 
+# satr"""
+# print(a)
+
+a='salom'
+#  01234
+print(a[1:5]) #1chi elemdan 4chi elemgacha
