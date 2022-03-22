0349bacb28fcea7553b04587b8a01ae79cac1d05
Umarjon <sharofiddinov.07@mail.ru>
2022-03-22 22:14:01 +0500

    Update 4dars.py

--- 4dars.py
+++ 4dars.py
@@ -3,6 +3,10 @@
 # satr"""
 # print(a)
 
-a='salom'
-#  01234
-print(a[1:5]) #1chi elemdan 4chi elemgacha
+# a='salom'
+# #  01234
+# print(a[1:5]) #1chi elemdan 4chi elemgacha
+
+#Strip - bo'sh joylarni ob tashlaydi
+a='  salomlar  '
+print(a.strip())
